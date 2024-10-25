8047c22b06fe6b7fd33c9a082018b78f15c9f2c3
Toney Abraham <Toney.Abraham@microchip.com>
2024-10-25 19:28:45 +0530

    updated the vendor-prefix.txt file with mchp prefix for microchip technology and updated every file and compatible with mchp

--- drivers/spi/spi_mchp_v1.c
+++ drivers/spi/spi_mchp_v1.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT microchip_v1_spi
+#define DT_DRV_COMPAT mchp_v1_spi
 
 #include "spi_context.h"
 #include <errno.h>
@@ -16,7 +16,7 @@
 #include <soc.h>
 
 /* Device constant configuration parameters */
-struct spi_microchip_v1_config {
+struct spi_mchp_v1_config {
 	SercomSpi *regs;
 	uint32_t pads;
 	const struct pinctrl_dev_config *pcfg;
@@ -38,14 +38,14 @@ struct spi_microchip_v1_config {
 };
 
 /* Device run time data */
-struct spi_microchip_v1_data {
+struct spi_mchp_v1_data {
 	struct spi_context ctx;
 #ifdef CONFIG_SPI_ASYNC
 	const struct device *dev;
 	uint32_t dma_segment_len;
 #endif
 };
-static int spi_microchip_v1_transceive_sync(const struct device *dev, const struct spi_config *config,
+static int spi_mchp_v1_transceive_sync(const struct device *dev, const struct spi_config *config,
 				       const struct spi_buf_set *tx_bufs,
 				       const struct spi_buf_set *rx_bufs)
 {
@@ -54,7 +54,7 @@ static int spi_microchip_v1_transceive_sync(const struct device *dev, const stru
     return 0;
 }
 
-static int spi_microchip_v1_release(const struct device *dev, const struct spi_config *config)
+static int spi_mchp_v1_release(const struct device *dev, const struct spi_config *config)
 {
 	// Release function
 	printf("This is the Spi release function");
@@ -62,7 +62,7 @@ static int spi_microchip_v1_release(const struct device *dev, const struct spi_c
 
 }
 
-static int spi_microchip_v1_init(const struct device *dev)
+static int spi_mchp_v1_init(const struct device *dev)
 {
 	// Initialize the spi
 	// printf("This is the spi Initialize");
@@ -70,52 +70,52 @@ static int spi_microchip_v1_init(const struct device *dev)
 
 }
 
-static const struct spi_driver_api spi_microchip_v1_driver_api = {
-	.transceive = spi_microchip_v1_transceive_sync,
-	.release = spi_microchip_v1_release,
+static const struct spi_driver_api spi_mchp_v1_driver_api = {
+	.transceive = spi_mchp_v1_transceive_sync,
+	.release = spi_mchp_v1_release,
 };
 
-#define SPI_MICROCHIP_V1_DMA_CHANNELS(n)
+#define SPI_MCHP_V1_DMA_CHANNELS(n)
 
-#define SPI_MICROCHIP_V1_SERCOM_PADS(n)                                                                 \
+#define SPI_MCHP_V1_SERCOM_PADS(n)                                                                 \
 	SERCOM_SPI_CTRLA_DIPO(DT_INST_PROP(n, dipo)) | SERCOM_SPI_CTRLA_DOPO(DT_INST_PROP(n, dopo))
 
 #ifdef MCLK
-#define SPI_MICROCHIP_V1_DEFINE_CONFIG(n)					\
-static const struct spi_microchip_v1_config spi_microchip_v1_config_##n = {		\
+#define SPI_MCHP_V1_DEFINE_CONFIG(n)					\
+static const struct spi_mchp_v1_config spi_mchp_v1_config_##n = {		\
 	.regs = (SercomSpi *)DT_INST_REG_ADDR(n),			\
 	.mclk = (volatile uint32_t *)MCLK_MASK_DT_INT_REG_ADDR(n),	\
 	.mclk_mask = BIT(DT_INST_CLOCKS_CELL_BY_NAME(n, mclk, bit)),	\
 	.gclk_core_id = DT_INST_CLOCKS_CELL_BY_NAME(n, gclk, periph_ch),\
-	.pads = SPI_MICROCHIP_V1_SERCOM_PADS(n),				\
+	.pads = SPI_MCHP_V1_SERCOM_PADS(n),				\
 	.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),			\
-	SPI_MICROCHIP_V1_DMA_CHANNELS(n)					\
+	SPI_MCHP_V1_DMA_CHANNELS(n)					\
 }
 #else
-#define SPI_MICROCHIP_V1_DEFINE_CONFIG(n)					\
-static const struct spi_microchip_v1_config spi_microchip_v1_config_##n = {		\
+#define SPI_MCHP_V1_DEFINE_CONFIG(n)					\
+static const struct spi_mchp_v1_config spi_mchp_v1_config_##n = {		\
 	.regs = (SercomSpi *)DT_INST_REG_ADDR(n),			\
 	.pm_apbcmask = BIT(DT_INST_CLOCKS_CELL_BY_NAME(n, pm, bit)),	\
 	.gclk_clkctrl_id = DT_INST_CLOCKS_CELL_BY_NAME(n, gclk, clkctrl_id),\
-	.pads = SPI_MICROCHIP_V1_SERCOM_PADS(n),				\
+	.pads = SPI_MCHP_V1_SERCOM_PADS(n),				\
 	.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),			\
-	SPI_MICROCHIP_V1_DMA_CHANNELS(n)					\
+	SPI_MCHP_V1_DMA_CHANNELS(n)					\
 }
 #endif /* MCLK */
 
-#define SPI_MICROCHIP_V1_DEVICE_INIT(n)                                                                 \
+#define SPI_MCHP_V1_DEVICE_INIT(n)                                                                 \
 	PINCTRL_DT_INST_DEFINE(n);                                                                 \
-	SPI_MICROCHIP_V1_DEFINE_CONFIG(n);                                                              \
-	static struct spi_microchip_v1_data spi_microchip_v1_dev_data_##n = {                                \
-		SPI_CONTEXT_INIT_LOCK(spi_microchip_v1_dev_data_##n, ctx),                              \
-		SPI_CONTEXT_INIT_SYNC(spi_microchip_v1_dev_data_##n, ctx),                              \
+	SPI_MCHP_V1_DEFINE_CONFIG(n);                                                              \
+	static struct spi_mchp_v1_data spi_mchp_v1_dev_data_##n = {                                \
+		SPI_CONTEXT_INIT_LOCK(spi_mchp_v1_dev_data_##n, ctx),                              \
+		SPI_CONTEXT_INIT_SYNC(spi_mchp_v1_dev_data_##n, ctx),                              \
 		SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(n), ctx)};                             \
-	DEVICE_DT_INST_DEFINE(n, spi_microchip_v1_init, NULL, &spi_microchip_v1_dev_data_##n,                \
-			      &spi_microchip_v1_config_##n, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,      \
-			      &spi_microchip_v1_driver_api);
+	DEVICE_DT_INST_DEFINE(n, spi_mchp_v1_init, NULL, &spi_mchp_v1_dev_data_##n,                \
+			      &spi_mchp_v1_config_##n, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,      \
+			      &spi_mchp_v1_driver_api);
 
-DT_INST_FOREACH_STATUS_OKAY(SPI_MICROCHIP_V1_DEVICE_INIT)
+DT_INST_FOREACH_STATUS_OKAY(SPI_MCHP_V1_DEVICE_INIT)
 
 // PINCTRL_DT_INST_DEFINE(n);       define pin ctrl from dts, configures cs,mosi,miso,sck
-// SPI_MICROCHIP_V1_DEFINE_CONFIG(n);    config struct for spi, init hardware specific values like reg,
+// SPI_MCHP_V1_DEFINE_CONFIG(n);    config struct for spi, init hardware specific values like reg,
 // clock, pads
